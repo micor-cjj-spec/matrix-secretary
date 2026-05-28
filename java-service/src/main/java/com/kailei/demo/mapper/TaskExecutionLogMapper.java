63a3bbdb431326c496f949927f91dce9a7daac06
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-05-28 16:22:27 +0800

    Add task execution log mapper

---
+++ java-service/src/main/java/com/kailei/demo/mapper/TaskExecutionLogMapper.java
@@ -0,0 +1,7 @@
+package com.kailei.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kailei.demo.entity.TaskExecutionLogEntity;
+
+public interface TaskExecutionLogMapper extends BaseMapper<TaskExecutionLogEntity> {
+}
