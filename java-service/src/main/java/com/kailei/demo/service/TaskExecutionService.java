d129e0bec8534092c2c077bdf975152faa07fcaa
micor <123868412+micor-cjj-spec@users.noreply.github.com>
2026-06-18 11:09:13 +0800

    feat: propagate trace id through task execution service

--- java-service/src/main/java/com/kailei/demo/service/TaskExecutionService.java
+++ java-service/src/main/java/com/kailei/demo/service/TaskExecutionService.java
@@ -34,31 +34,39 @@ public class TaskExecutionService {
     }
 
     public TaskAction confirmAction(String planId, String userId, TaskAction action, String operatorUserId) {
+        return confirmAction(planId, null, userId, action, operatorUserId);
+    }
+
+    public TaskAction confirmAction(String planId, String traceId, String userId, TaskAction action, String operatorUserId) {
         if (stateMachineService.canSchedule(action)) {
             TaskAction scheduledAction = action.withSchedule(cronScheduleService.ensureCronAndNextRun(action.schedule()));
             String note = "已进入调度队列: cron=" + scheduledAction.schedule().cron()
                     + ", nextRunAt=" + scheduledAction.schedule().effectiveRunAt();
             log.info("Schedule action [{}] {}", action.actionId(), note);
             TaskAction next = scheduledAction.withStatus(TaskStatus.SCHEDULED, note);
-            executionLogRepository.logStateChange(planId, action, next, operatorUserId);
+            executionLogRepository.logStateChange(planId, traceId, action, next, operatorUserId);
             return next;
         }
-        return executeNow(planId, userId, action, operatorUserId);
+        return executeNow(planId, traceId, userId, action, operatorUserId);
     }
 
     public TaskAction confirmAction(String planId, TaskAction action, String operatorUserId) {
-        return confirmAction(planId, operatorUserId, action, operatorUserId);
+        return confirmAction(planId, null, operatorUserId, action, operatorUserId);
     }
 
     public TaskAction executeNow(String planId, String userId, TaskAction action, String operatorUserId) {
+        return executeNow(planId, null, userId, action, operatorUserId);
+    }
+
+    public TaskAction executeNow(String planId, String traceId, String userId, TaskAction action, String operatorUserId) {
         stateMachineService.ensureExecutable(action);
         SkillDefinition skill = skillCatalog.getOrUnknown(action.actionType());
         TaskAction next = genericSkillExecutor.execute(planId, userId, skill, action);
-        executionLogRepository.logStateChange(planId, action, next, operatorUserId);
+        executionLogRepository.logStateChange(planId, traceId, action, next, operatorUserId);
         return next;
     }
 
     public TaskAction executeNow(String planId, TaskAction action, String operatorUserId) {
-        return executeNow(planId, operatorUserId, action, operatorUserId);
+        return executeNow(planId, null, operatorUserId, action, operatorUserId);
     }
 }
